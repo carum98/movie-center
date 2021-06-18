c0c17fe4b83479ea586d076dd0a99b2fefc5886b
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-17 19:32:00 -0600

    Network and Model

---
+++ MovieCenter/Models/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieCenter
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 17/6/21.
+//
+
+import Foundation
+
+struct Movies : Codable {
+    var originalTitle : String
+    var overview : String
+}
