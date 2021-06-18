6220394e0cdd79cd0fd43d59ef8de4e872c48b91
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-17 18:49:46 -0600

    Initial Commit

---
+++ MovieCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieCenter
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 17/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieCenter/MovieCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCenterApp.swift
+//  MovieCenter
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 17/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
