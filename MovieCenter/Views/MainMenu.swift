3db32993427e336d5d8727f45290aeb48c659e63
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-22 22:05:22 -0600

    Main menu

--- MovieCenter/Views/MainMenu.swift
+++ MovieCenter/Views/MainMenu.swift
@@ -11,17 +11,20 @@ struct MainMenu: View {
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
                 NavigationLink(
                     destination: MoviesList(),
                     label: {
-                        Text("Peliculas")
+                        CustomButton(label: "Peliculas", icon: "film")
                     })
+                Spacer()
                 NavigationLink(
                     destination: TVShowList(),
                     label: {
-                        Text("Series")
+                        CustomButton(label: "Series", icon: "tv")
                     })
-            }
+                Spacer()
+            }.navigationTitle("MovieCenter")
         }
     }
 }
@@ -29,5 +32,29 @@ struct MainMenu: View {
 struct MainMenu_Previews: PreviewProvider {
     static var previews: some View {
         MainMenu()
+            .preferredColorScheme(.dark)
+            
+    }
+}
+
+struct CustomButton: View {
+    let label : String
+    let icon : String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: icon)
+            Text(label)
+                .bold()
+        }
+        .frame(width: 200.0)
+        .font(.system(size: 40))
+        .padding(40)
+        .cornerRadius(40)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10.0)
+                .stroke(lineWidth: 2.0)
+                .shadow(color: .blue, radius: 10.0)
+    )
     }
 }
